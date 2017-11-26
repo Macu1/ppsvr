f347f89180c287cbc3b20356f939cb2d03a452c1
Macu1 <289602744@qq.com>
2017-11-26 18:55:48 +0200

    add nothing

---
+++ apps/web/src/web_app.erl
@@ -0,0 +1,31 @@
+%% Feel free to use, reuse and abuse the code in this file.
+
+%% @private
+-module(web_app).
+-behaviour(application).
+
+%% API.
+-export([start/2]).
+-export([stop/1]).
+
+%% API.
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/hello_world", hello_world_handler, []},
+			{"/", cowboy_static, {priv_file, web, "index.html"}},
+			{"/websocket", ws_handler, []},
+			{"/static/[...]", cowboy_static, {priv_dir, web, "static"}}
+		]}
+	]),
+	{ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
+		env => #{dispatch => Dispatch}
+	}),
+    service_start_link().
+
+service_start_link()->
+    hello_world_sup:start_link(),
+	websocket_sup:start_link().
+
+stop(_State) ->
+	ok.
